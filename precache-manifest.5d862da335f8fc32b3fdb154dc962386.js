e78c159b312c008277352cb7060084bac0c7a493
Sujin-Im <limsj0223@gmail.com>
2019-11-25 13:47:56 +0900

    Updates

---
+++ precache-manifest.5d862da335f8fc32b3fdb154dc962386.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cebd13afbd892dba928404a49facfc63",
+    "url": "/movie_app_2019/index.html"
+  },
+  {
+    "revision": "a3f04188d0ae11cbb98a",
+    "url": "/movie_app_2019/static/css/main.ae4cfe19.chunk.css"
+  },
+  {
+    "revision": "2bed164abecb2001a4f8",
+    "url": "/movie_app_2019/static/js/2.14ba9984.chunk.js"
+  },
+  {
+    "revision": "a3f04188d0ae11cbb98a",
+    "url": "/movie_app_2019/static/js/main.e965429b.chunk.js"
+  },
+  {
+    "revision": "44901d82ce6105999ea4",
+    "url": "/movie_app_2019/static/js/runtime-main.0f07e758.js"
+  }
+]);
